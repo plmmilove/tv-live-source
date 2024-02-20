ba310d3840d7395905ff77c3c6febb5aea0d0254
plmmilove <plmmilove@foxmail.com>
2024-02-20 13:09:15 +0800

    update

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,7 +7,7 @@ Cypress.Commands.add('parseChannel', (html, index) => {
         let name = $(el).children('div.channel').find('div:eq(0)').text().trim()
         let url = $(el).children('div.m3u8').find('td:eq(1)').text().trim()
 
-        if(name.length === 0 || url.length === 0  || name.includes('画中画') || name.includes('单音轨')) {
+        if(name.length === 0 || url.length === 0  || name.includes('画中画') || name.includes('单音轨') || name.includes('热门')) {
             return
         }
 
