5acae78fc521e2128d4b3074741d03ad1b100d84
plmmilove <plmmilove@foxmail.com>
2024-02-23 12:00:52 +0800

    update

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 Cypress.Commands.add('getMulticastIps', () => {
     let ips = Cypress.env('multicast_ips')
     let ipArray
-    if (ips == null) {
+    if (ips == null || ips.length === 0) {
         ipArray = cy.request('http://tonkiang.us/hoteliptv.php?s=' + encodeURIComponent('四川电信'))
             .then((response) => {
                 return Cypress.$(response.body).find('div.result:lt(3) > div.channel > a').map((i, el) => {
