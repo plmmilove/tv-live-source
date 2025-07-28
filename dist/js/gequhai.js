ab46428092f2ee408aec30230dbce5ed5439fbd4
plmmilove <plmmilove@foxmail.com>
2025-07-28 20:47:53 +0800

    Update gequhai.js

--- dist/js/gequhai.js
+++ dist/js/gequhai.js
@@ -2,8 +2,8 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 module.exports = {
-  platform: '歌曲海',
-  version: '1.0.0',
+  platform: '歌曲海2',
+  version: '1.1.0',
   cacheControl: 'no-store',
   async search(query, page, type) {
     if (type === 'music') {
@@ -47,13 +47,18 @@ module.exports = {
       await axios.get(`https://www.gequhai.com/play/${musicBase.id}`)
     ).data
 
+    const playIdReg = /play_id\s*=\s*['"]([^'"]+)['"]/
     const coverReg = /mp3_cover\s*=\s*['"]([^'"]+)['"]/
     const titleReg = /mp3_title\s*=\s*['"]([^'"]+)['"]/
     const authorReg = /mp3_author\s*=\s*['"]([^'"]+)['"]/
 
+    const playId = rawHtml.match(playIdReg)
     const cover = rawHtml.match(coverReg)
     const title = rawHtml.match(titleReg)
     const author = rawHtml.match(authorReg)
+
+    const $ = cheerio.load(rawHtml);
+    const lrc = $('#content-lrc2').text()
 
     return {
         artist: author[1],
@@ -61,23 +66,13 @@ module.exports = {
         duration: 300,
         /** 专辑封面图 */
         artwork: cover[1],
+        playId: playId[1],
+        lrc: lrc,
     }
   },
 
   async getMediaSource(musicItem, quality) {
-    const rawHtml = (
-      await axios.get(`https://www.gequhai.com/play/${musicItem.id}`)
-    ).data
-
-    const playIdReg = /play_id\s*=\s*['"]([^'"]+)['"]/
-
-    const playId = rawHtml.match(playIdReg)
-
-    if (!playId) {
-      throw new Error('无法找到播放ID')
-    }
-
-    let data = `id=${encodeURIComponent(playId[1])}`
+    let data = `id=${encodeURIComponent(musicItem.playId)}`
 
     let config = {
       method: 'post',
