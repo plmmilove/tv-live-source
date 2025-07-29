930a940cd2865c776885a4a32a73054de08f64bc
plmmilove <plmmilove@users.noreply.github.com>
2025-07-29 07:05:00 +0000

    deploy: ffd38d920fd84a5f2a2fe8e3d388e192246e688e

--- js/wuyou.js
+++ js/wuyou.js
@@ -5,10 +5,12 @@ module.exports = {
     platform: "无忧",
     version: "1.0.0",
     cacheControl: "no-store",
+    cache: new Map(),
     async search(query, page, type) {
         if (type === "music") {
+            query = encodeURIComponent(query)
             const rawHtml = (
-                await axios.get(`https://www.qeecc.com/so/${query}/${page}.html`)
+              await axios.get(`https://www.qeecc.com/so/${query}/${page}.html`)
             ).data;
 
             const $ = cheerio.load(rawHtml);
@@ -39,7 +41,7 @@ module.exports = {
 
     async getMusicInfo(musicBase) {
         const rawHtml = (
-            await axios.get(`https://www.qeecc.com/song/${musicBase.id}.html`)
+          await axios.get(`https://www.qeecc.com/song/${musicBase.id}.html`)
         ).data
 
         const $ = cheerio.load(rawHtml);
@@ -76,9 +78,48 @@ module.exports = {
 
         data = JSON.parse(data)
 
+        if (this.cache.size === 100) {
+            this.cache.delete(this.cache.keys().next().value)
+        }
+        this.cache.set(musicItem.id, data.lkid)
+
         return {
             url: data.url,
             quality: "standard"
         };
+    },
+
+    async getLyric(musicItem) {
+        const cid = this.cache.get(musicItem.id)
+        if(!cid) {
+            return null
+        }
+
+        const resp = await fetch(`https://js.eev3.com/lrc.php?cid=${cid}`, {
+            "headers": {
+                "accept": "application/json, text/javascript, */*; q=0.01",
+                "accept-language": "zh-CN,zh;q=0.9,ko;q=0.8",
+                "cache-control": "no-cache",
+                "pragma": "no-cache",
+                "priority": "u=1, i",
+                "sec-ch-ua": "\"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Google Chrome\";v=\"138\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "cross-site"
+            },
+            "body": null,
+            "method": "GET",
+            "mode": "cors",
+            "credentials": "omit"
+        });
+
+        let data = await resp.text()
+        data = JSON.parse(data)
+
+        return {
+            rawLrc: data.lrc,
+        }
     },
 };
