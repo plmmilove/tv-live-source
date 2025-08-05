65441b2d5cc1e7fb0053afc30b1f5031ca025909
plmmilove <plmmilove@users.noreply.github.com>
2025-08-05 01:17:28 +0000

    deploy: 11118558bbca7474c98531ee87ea337441938ab8

--- js/33ve.js
+++ js/33ve.js
@@ -10,7 +10,7 @@ module.exports = {
     if (type === 'music') {
       query = encodeURIComponent(query)
 
-      const { isEnd, musicList } = await this.getMusicList(
+      const {isEnd, musicList} = await this.getSearchMusicList(
         `https://www.33ve.com/so.php?wd=${query}&page=${page}`)
       return {
         isEnd: isEnd,
@@ -39,61 +39,59 @@ module.exports = {
   async getTopLists() {
     return [{
       data: [
-        {id: 'hot', title: '网络红歌榜'},
-        {id: 'new', title: '网络最新榜'},
-        {id: 'top', title: 'TOP榜'},
-        {id: 'djwuqu', title: 'DJ舞曲大全'},
-        {id: 'share', title: '音乐热评榜'},
-        {id: 'ndtop', title: '音乐先锋榜'},
-        {id: 'hktop', title: '爱听电音榜'},
-        {id: 'cztop', title: '车载歌曲榜'},
-        {id: 'ygtop', title: '英国排行榜'},
-        {id: 'krtop', title: '韩国排行榜'},
-        {id: 'jptop', title: '日本排行榜'},
-        {id: 'kuaishou', title: '快手热歌榜'},
+        {id: 'kugou', title: '酷狗飙升榜'},
+        {id: 'top', title: 'TOP排行榜'},
         {id: 'douyin', title: '抖音热歌榜'},
-        {id: 'kwyc', title: '酷我原创榜'},
+        {id: 'kuaishou', title: '快手热歌榜'},
+        {id: 'djtop', title: 'DJ热歌榜'},
+        {id: 'share', title: '音乐分享榜'},
+        {id: 'ndtop', title: '内地排行榜'},
+        {id: 'hktop', title: '香港排行榜'},
+        {id: 'twtop', title: '台湾排行榜'},
+        {id: 'ustop', title: '欧美排行榜'},
+        {id: 'krtop', title: '韩国排行榜'},
         {id: 'newacg', title: 'ACG新歌榜'},
-        {id: 'kuwo', title: '酷我飙升榜'},
         {id: 'dytop', title: '电音热歌榜'},
         {id: 'newzy', title: '综艺新歌榜'},
         {id: 'sctop', title: '说唱先锋榜'},
         {id: 'ystop', title: '影视金曲榜'},
         {id: 'yytop', title: '粤语金曲榜'},
-        {id: 'ustop', title: '欧美金曲榜'},
+        {id: 'omtop', title: '欧美金曲榜'},
+        {id: 'kkyc', title: '音乐人原创榜'},
+        {id: 'leida', title: '酷狗雷达榜'},
         {id: 'blhot', title: '80后热歌榜'},
-        {id: 'wlhot', title: '网红新歌榜'},
-        {id: 'gfhot', title: '古风音乐榜'},
-        {id: 'xrtop', title: '夏日畅爽榜'},
-        {id: 'vip', title: '会员喜爱榜'},
-        {id: 'jstop', title: '跑步健身榜'},
-        {id: 'bbtop', title: '宝宝哄睡榜'},
-        {id: 'sqtop', title: '睡前放松榜'},
-        {id: 'aytop', title: '熬夜修仙榜'},
-        {id: 'vlogtop', title: 'Vlog必备榜'},
-        {id: 'ktvtop', title: 'KTV点唱榜'},
-        {id: 'tqltop', title: '通勤路上榜'},
+        {id: 'jlhot', title: '90后热歌榜'},
+        {id: 'mgtop', title: '美国单曲榜'},
+        {id: 'ygtop', title: '英国单曲榜'},
+        {id: 'rbtop', title: '日本歌曲榜'},
+        {id: 'hgtop', title: '韩国音乐榜'},
+        {id: 'joox', title: 'JOOX热歌榜'},
+        {id: 'kkbox', title: 'KKBOX风云榜'},
+        {id: 'space', title: 'SHOWER榜'},
+        {id: 'btptop', title: '电子舞曲榜'},
+        {id: 'xyztop', title: '小语种热歌'},
+        {id: 'song', title: '洗脑神曲'},
+        {id: 'djwuqu', title: 'DJ舞曲'},
       ],
     }]
   },
 
   async getTopListDetail(topListItem, page) {
-    let url = `https://www.qeecc.com/list/${topListItem.id}/${page}.html`
+    let url = `https://www.33ve.com/list/${topListItem.id}/${page}.html`
     return await this.getMusicList(url)
   },
 
   async getRecommendSheetTags() {
     const rawHtml = (
-      await axios.get(`https://www.qeecc.com/playtype/index.html`)
+      await axios.get(`https://www.33ve.com/gdlist/index.html`)
     ).data
 
     const $ = cheerio.load(rawHtml)
     const data = []
-    const rootElements = $('div.ilingku_fl')
+    const rootElements = $('div.class')
     rootElements.each((index, element) => {
-      const title = $(element).find('li:eq(0)').text().trim().slice(0,
-        -1).trim()
-      const resultElements = $(element).find('li > a')
+      const title = $(element).find('h1').text().trim()
+      const resultElements = $(element).find('ul > li > a')
 
       const subData = []
       resultElements.each((si, se) => {
@@ -101,7 +99,7 @@ module.exports = {
 
         const href = e.attr('href')
         const subTitle = e.text()
-        const id = href.slice(10, -5)
+        const id = href.slice(8, -5)
 
         subData.push({
           id,
@@ -122,7 +120,7 @@ module.exports = {
   },
 
   async getRecommendSheetsByTag(tag, page) {
-    let url = `https://www.qeecc.com/playtype/${tag.id}/${page}.html`
+    let url = `https://www.33ve.com/gdlist/${tag.id}/${page}.html`
 
     const rawHtml = (
       await axios.get(url)
@@ -131,7 +129,7 @@ module.exports = {
     const $ = cheerio.load(rawHtml)
 
     const list = []
-    const resultElements = $('div.video_list > ul.play > li')
+    const resultElements = $('div.lkpic_list > ul > li')
 
     for (let i = 0; i < resultElements.length; i++) {
       const e = $(resultElements[i])
@@ -139,7 +137,7 @@ module.exports = {
       const href = e.find('div.pic > a').attr('href')
       const id = href.slice(10, -5)
 
-      const title = e.find('div.name > a').attr('title')
+      const title = e.find('div.name > a:eq(0)').text()
       const coverImg = e.find('div.pic > a > img').attr('src')
 
       list.push({
@@ -158,7 +156,42 @@ module.exports = {
 
   async getMusicSheetInfo(sheetBase, page) {
     return await this.getMusicList(
-      `https://www.qeecc.com/playlist/${sheetBase.id}/${page}.html`)
+      `https://www.33ve.com/playlist/${sheetBase.id}/${page}.html`)
+  },
+
+  async getSearchMusicList(url) {
+    const rawHtml = (
+      await axios.get(url)
+    ).data
+
+    const $ = cheerio.load(rawHtml)
+    const list = []
+    const resultElements = $('.play_list > ul > li')
+    resultElements.each((index, element) => {
+      const e = $(element).find('div.pic > a')
+      const href = e.attr('href')
+
+      const id = href.slice(5, -5)
+      const title = $(e).find('img').attr('alt')
+      const name = $(element).find('div.name > a:eq(0)').text()
+
+      const artist = name.substring(0, name.indexOf('-')).trim()
+      const artwork = $(e).find('img').attr('src')
+
+      list.push({
+        id,
+        title,
+        artist,
+        artwork,
+        duration: 300,
+        platform: this.platform,
+      })
+    })
+
+    return {
+      isEnd: $('div.page > a:contains("下一页")').length === 0,
+      musicList: list,
+    }
   },
 
   async getMusicList(url) {
@@ -199,25 +232,25 @@ module.exports = {
   async getPostData(id) {
     let data = this.cache.get(id)
     if (!data) {
-      const resp = await fetch("https://www.33ve.com/style/js/play.php", {
-        "headers": {
-          "accept": "application/json, text/javascript, */*; q=0.01",
-          "accept-language": "zh-CN,zh;q=0.9,ko;q=0.8",
-          "cache-control": "no-cache",
-          "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-          "pragma": "no-cache",
-          "priority": "u=1, i",
-          "sec-ch-ua": "\"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Google Chrome\";v=\"138\"",
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": "\"Windows\"",
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-origin",
-          "x-requested-with": "XMLHttpRequest",
-          "Referer": `https://www.33ve.com/mp3/${id}.html`
+      const resp = await fetch('https://www.33ve.com/style/js/play.php', {
+        'headers': {
+          'accept': 'application/json, text/javascript, */*; q=0.01',
+          'accept-language': 'zh-CN,zh;q=0.9,ko;q=0.8',
+          'cache-control': 'no-cache',
+          'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'pragma': 'no-cache',
+          'priority': 'u=1, i',
+          'sec-ch-ua': '"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-origin',
+          'x-requested-with': 'XMLHttpRequest',
+          'Referer': `https://www.33ve.com/mp3/${id}.html`,
         },
-        "body": `id=${id}&type=dance`,
-        "method": "POST"
+        'body': `id=${id}&type=dance`,
+        'method': 'POST',
       })
 
       data = await resp.json()
